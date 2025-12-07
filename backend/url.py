06a0bbc9c159e19dd1919df49339dbc55d594e2a
Neelraj <subhash.nitta@gmail.com>
2025-12-07 18:01:23 -0500

    url to fetch unique sector in S&P added

--- backend/url.py
+++ backend/url.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import *
+from .dataset_views import sp500_gics_sectors
 urlpatterns = [
     path('test/', test, name='test'),
     path('train_model/', train_model, name='train_model'),
     path('list_tickers/', list_tickers, name='list_tickers'),
+    path('train_by_sectors/', train_by_sectors, name='train_by_sectors'),
+    path('sp500/gics-sectors/', sp500_gics_sectors, name='sp500_gics_sectors'),
 ]
